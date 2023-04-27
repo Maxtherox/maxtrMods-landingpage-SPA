877bcb76d2cbb7da49d98f90bff7d84722b9a27d
Guilherme <gui-rock02@Hotmail.com>
2023-04-27 17:54:31 -0300

    Home finalizado

---
+++ src/javascript/index.js
@@ -0,0 +1,10 @@
+let prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+let currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-65px";
+  }
+  prevScrollpos = currentScrollPos;
+}
